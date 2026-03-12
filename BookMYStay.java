ea3538a2488f715c4afba33df601cd71410eee83
sankalpMohan <sm3746@srmist.edu.in>
2026-03-12 09:38:31 +0530

    UC1: Add welcome message module

---
+++ BookMYStay.java
@@ -0,0 +1,7 @@
+public class HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Booking Application");
+        System.out.println("System is starting...");
+        System.out.println("Initializing hotel booking services...");
+    }
+}
